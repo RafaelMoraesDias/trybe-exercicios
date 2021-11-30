4f5ae8ab9b0fff5e48ec745bdb41c286ab7553ec
Rafael Moraes Dias <rafasjbvb@yahoo.com.br>
2021-11-30 19:04:17 -0300

    exercicio 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/logica-de-programacao/exercicios4-3.js
@@ -0,0 +1,13 @@
+//Exercicio 1
+
+let numero = 5;
+let linha= "*";
+let coluna = " ";
+
+for (let l = 1; l <= numero; l +=1 ){
+     coluna += linha;   
+  
+}
+for (let c = 1; c <= numero; c +=1 ){
+    console.log (coluna);
+}
